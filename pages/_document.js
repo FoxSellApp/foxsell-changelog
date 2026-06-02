b7ede62ae2b0b37a1cbf88172b9f4b8ffe534b73
knox-the-fox <knox@foxsell.app>
2026-06-02 15:01:29 +0000

    fix: refresh favicon assets

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,12 @@ export default function Document() {
           name="description"
           content="Merchant-facing product updates, launches, and improvements from FoxSell."
         />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="icon" href="/favicon-32x32.png" type="image/png" sizes="32x32" />
+        <link rel="icon" href="/favicon-16x16.png" type="image/png" sizes="16x16" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" sizes="180x180" />
+        <link rel="manifest" href="/site.webmanifest" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link
